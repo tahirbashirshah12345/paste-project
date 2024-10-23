302f93eb125f5380a4beb454c3f8bf533b650f64
project demo <shahtahir3174@gmail.com>
2024-10-24 00:40:08 +0530

    initial setup complete

---
+++ paste/src/App.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+// import Home from "./components/Home"
+// import Paste from "./components/Paste"
+// import ViewPaste from "./components/ViewPaste"
+import Navbar from "./components/Navbar"
+import Paste from "./components/Paste"
+import Home from "./components/Home"
+import ViewPaste from "./components/VeiwPaste"
+
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:
+      <div className="w-full h-full flex flex-col">
+        <Navbar/>
+        <Home/>
+      </div>
+    },
+    {
+      path:"/pastes",
+      element: <div className="w-full h-full flex flex-col">
+      <Navbar/>
+      <Paste/>
+    </div>
+    },
+    {
+      path:"/pastes/:id",
+      element: <div className="w-full h-full flex flex-col">
+      <Navbar/>
+      <ViewPaste/>
+    </div>,
+    }
+  ]
+)
+
+function App() {
+
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
